22d91ff6a6ac0d7fffd62837dbc6e556cb8f180f
umfrancisco <francis.guitler@gmail.com>
2026-06-21 13:11:40 -0300

    Change API base URL to production server

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://ec2-54-232-251-129.sa-east-1.compute.amazonaws.com:8080/",
   timeout: 5000,
 });
 
